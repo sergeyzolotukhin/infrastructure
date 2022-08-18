4387ce02eb6cd19572e51f4716b72c5be9355f2b
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2022-08-19 00:02:04 +0300

    docker engine

---
+++ apache2/README.txt
@@ -0,0 +1,2 @@
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout apache-selfsigned.key -out apache-selfsigned.crt -subj "/C=UA/ST=Kyiv/L=Kyiv/O=Home/CN=ubuntu-focal"
--- apache2/install-apache2.sh
+++ apache2/install-apache2.sh
@@ -1,12 +1,21 @@
 echo 'Apt update ======================================================================================================'
 sudo apt-get update
 
-echo 'Apache install =================================================================================================='
-sudo apt-get -y install apache2
-sudo cp -r /vagrant/var/www/html/* /var/www/html
-
 echo 'Install tools ==================================================================================================='
 sudo apt-get -y install mc
 
 echo 'Apache install =================================================================================================='
+sudo apt-get -y install apache2
+sudo cp -r /vagrant/var/www/html/* /var/www/html
+sudo cp -r /vagrant/etc/apache2/conf-available/ssl-params.conf /etc/apache2/conf-available/ssl-params.conf
+sudo cp -r /vagrant/etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
+sudo cp -r /vagrant/etc/ssl/certs/apache-selfsigned.crt /etc/ssl/certs/apache-selfsigned.crt
+sudo cp -r /vagrant/etc/ssl/private/apache-selfsigned.key /etc/ssl/private/apache-selfsigned.key
+sudo a2enmod ssl
+sudo a2enmod headers
+sudo a2enconf ssl-params
+sudo a2ensite default-ssl
+sudo apache2ctl configtest
+
+echo 'Apache restart =================================================================================================='
 sudo systemctl restart apache2
