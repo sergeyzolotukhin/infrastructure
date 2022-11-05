3e5de9669452c260c5ca8dd66254d1fffb9bd133
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2022-11-05 20:42:14 +0200

    Docker CE

--- docker-engine-shell/docker-engine.sh
+++ docker-engine-shell/docker-engine.sh
@@ -1,7 +1,7 @@
 echo '================================================================================================================'
 echo 'Add docker repository'
 echo '================================================================================================================'
-sudo apt -y install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` test"
 
