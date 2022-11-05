b4b2bba99f5e33ad72bee69b0e0776c19e8fa05a
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2022-11-05 20:49:22 +0200

    Docker CE

--- docker-engine-shell/docker-engine.sh
+++ docker-engine-shell/docker-engine.sh
@@ -1,7 +1,6 @@
 echo '================================================================================================================'
 echo 'Add docker repository'
 echo '================================================================================================================'
-sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu `lsb_release -cs` test"
 
@@ -11,9 +10,6 @@ echo '==========================================================================
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
-#printf '%80s\n' | tr " " "="
-#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-#apt-cache madison docker-ce
-
-#sudo apt-get install -y docker-ce=5:20.10.17~3-0~ubuntu-focal docker-ce-cli=5:20.10.17~3-0~ubuntu-focal containerd.io docker-compose-plugin
-#sudo docker run hello-world
+echo '================================================================================================================'
+echo 'Docker engine has installed'
+echo '================================================================================================================'
