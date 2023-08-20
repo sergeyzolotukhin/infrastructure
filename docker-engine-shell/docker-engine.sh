9d8172a0fc38ea25dad7d40835d6f82857b64da1
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-08-20 14:40:47 +0300

    Docker CE

--- docker-engine-shell/docker-engine.sh
+++ docker-engine-shell/docker-engine.sh
@@ -11,6 +11,14 @@ sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 #sudo apt-get -y install docker-compose-plugin
 
+echo 'Enable remote access to docker'
+sudo cp /vagrant/lib/systemd/system/docker.service /lib/systemd/system/docker.service
+
+echo 'Restart docker'
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+#sudo systemctl status docker
+
 echo '================================================================================================================'
 echo 'Docker engine has installed'
 echo '================================================================================================================'
