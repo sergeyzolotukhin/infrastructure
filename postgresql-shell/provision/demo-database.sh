6c011b32dbb6ace688cd40a2fa0067f1edda1f45
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-08-27 20:56:28 +0300

    Docker CE

--- postgresql-shell/provision/demo-database.sh
+++ postgresql-shell/provision/demo-database.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DATA_HOME='/var/lib/postgresql/14'
+
 echo "================================================================================================================"
 
 echo "Drop database if exists"
@@ -11,14 +13,14 @@ sudo -u postgres psql -c "DROP TABLESPACE IF EXISTS demo_tablespace"
 sudo -u postgres psql -c "DROP TABLESPACE IF EXISTS demo_tablespace_01"
 
 echo "Remove folder for tablespace if exists"
-if [ -d "/var/lib/postgresql/14/demo_tablespace" ];
+if [ -d "${DATA_HOME}/demo_tablespace" ];
 then
-  sudo -u postgres rm -Rf /var/lib/postgresql/14/demo_tablespace;
+  sudo -u postgres rm -Rf $DATA_HOME/demo_tablespace;
 fi
 
-if [ -d "/var/lib/postgresql/14/demo_tablespace_01" ];
+if [ -d "${DATA_HOME}/demo_tablespace_01" ];
 then
-  sudo -u postgres rm -Rf /var/lib/postgresql/14/demo_tablespace_01;
+  sudo -u postgres rm -Rf $DATA_HOME/demo_tablespace_01;
 fi
 
 echo "Create folder for tablespace"
@@ -26,8 +28,8 @@ sudo -u postgres mkdir /var/lib/postgresql/14/demo_tablespace
 sudo -u postgres mkdir /var/lib/postgresql/14/demo_tablespace_01
 
 echo "Create tablespace"
-sudo -u postgres psql -c "CREATE TABLESPACE demo_tablespace    OWNER postgres LOCATION '/var/lib/postgresql/14/demo_tablespace'"
-sudo -u postgres psql -c "CREATE TABLESPACE demo_tablespace_01 OWNER postgres LOCATION '/var/lib/postgresql/14/demo_tablespace_01'"
+sudo -u postgres psql -c "CREATE TABLESPACE demo_tablespace    OWNER postgres LOCATION '${DATA_HOME}/demo_tablespace'"
+sudo -u postgres psql -c "CREATE TABLESPACE demo_tablespace_01 OWNER postgres LOCATION '${DATA_HOME}/demo_tablespace_01'"
 
 
 echo "Recreate database"
