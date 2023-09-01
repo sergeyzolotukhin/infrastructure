059a916e9d7a69d5e9945f55ce3b8922055af723
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-09-02 00:17:02 +0300

    Docker CE

--- docker-engine-shell/examples/color-text.sh
+++ docker-engine-shell/examples/color-text.sh
@@ -3,3 +3,16 @@
 source ./color_constants.sh
 
 printf "${RED}complete!${CB}\n"
+
+s=20
+for ((k = 0; k <= s ; k++))
+do
+    echo -n "[ "
+    for ((i = 0 ; i <= k; i++)); do echo -n "###"; done
+    for ((j = i ; j <= s ; j++)); do echo -n "   "; done
+    v=$((k * 5))
+    echo -n " ] "
+    echo -n "$v %" $'\r'
+    sleep 0.05
+done
+echo
