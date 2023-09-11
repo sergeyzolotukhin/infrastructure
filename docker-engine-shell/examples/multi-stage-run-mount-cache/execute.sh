5b07073472168d87148a8d28d7b516562c328f2d
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-09-12 00:42:33 +0300

    Docker CE

---
+++ docker-engine-shell/examples/multi-stage-run-mount-cache/execute.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#echo "program $0 arg "
+#echo $@
+#echo "UBUNTU_VERSION: ${UBUNTU_VERSION}"
+#echo "CUDA_VERSION: ${CUDA_VERSION}"
+
+#echo "================================================================================================================"
+#printenv
+#echo "================================================================================================================"
+
+ls -l /mnt
+
+echo "Hello World" > "/mnt/hello-$( date '+%F_%H:%M:%S' ).txt"
+
+#mkdir --parents /mnt/child
+
