75fb74d24f5c0b348a1bc3bf01f3c2ed6f0c30fd
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2023-09-16 13:50:29 +0300

    Docker CE

--- jfrog-artifactory-shell/install-jfrog-artifactory.sh
+++ jfrog-artifactory-shell/install-jfrog-artifactory.sh
@@ -12,15 +12,15 @@ sudo cp /vagrant/jfrog-artifactory-oss-7.39.4-linux.tar.gz /opt/jfrog-artifactor
 cd /opt/jfrog-artifactory || return
 
 #echo 'Set the JFrog Home'
-#export JFROG_HOME=/opt/jfrog-artifactory
+export JFROG_HOME=/opt/jfrog-artifactory
 
 echo 'Extract installer archive'
 sudo tar -xf jfrog-artifactory-oss-7.39.4-linux.tar.gz
 sudo mv artifactory-oss-7.39.4 artifactory
 
 #echo 'Install service JFrog Artifactory'
-#sudo /opt/jfrog-artifactory/artifactory/app/bin/installService.sh
+sudo /opt/jfrog-artifactory/artifactory/app/bin/installService.sh
 
 #echo 'Restart JFrog Artifactory'
-#sudo systemctl start artifactory.service
-#sudo systemctl enable artifactory.service
+sudo systemctl start artifactory.service
+sudo systemctl enable artifactory.service
