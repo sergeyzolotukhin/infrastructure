cb5d4cd6bdf100b11acddbe0f9b95bfc4d4f46b6
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-03-22 21:05:43 +0200

    artemis

--- java-shell/install-amazon-java-manual.sh
+++ java-shell/install-amazon-java-manual.sh
@@ -8,7 +8,7 @@ sudo apt-get -y install mc
 
 echo 'Install Java ===================================================================================================='
 sudo apt-get install java-common
-sudo dpkg --install /vagrant/java-1.8.0-amazon-corretto-jdk_8.342.07-4_amd64.deb
+sudo dpkg --install /vagrant/java-21-amazon-corretto-jdk_21.0.6.7-1_amd64.deb
 
 echo 'Verify Java JDK Installation====================================================================================='
 sudo java -version
