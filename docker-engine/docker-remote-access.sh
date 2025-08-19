671e3584f9100232fc88ba5f6155256d29431acc
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-08-19 23:19:25 +0300

    IDEA Remote development

--- docker-engine/docker-remote-access.sh
+++ docker-engine/docker-remote-access.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+echo 'Stop Docker'
+sudo systemctl stop docker.socket
+sudo systemctl stop docker
+
+echo 'Enable remote access'
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo cp /vagrant/etc/systemd/system/docker.service.d/override.conf /etc/systemd/system/docker.service.d/override.conf
 
+echo 'Start Docker'
 sudo systemctl daemon-reload
-
-sudo systemctl --no-pager status docker
-sudo systemctl stop docker
 sudo systemctl start docker
+sudo systemctl start docker.socket
+
+#echo 'Status Docker'
+#sudo systemctl --no-pager status docker
