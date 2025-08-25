b6cc9d0fce397cd00e252413e2a704aca2c8338a
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-08-26 00:05:46 +0300

    IDEA Remote development

---
+++ docker-engine/shell/file-menu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Bash Menu Script Example
+
+echo "The following `*.war` archives were found; select one:"
+
+# set the prompt used by select, replacing "#?"
+PS3="Use number to select a file or 'stop' to cancel: "
+
+# allow the user to choose a file
+select filename in *.sh
+do
+    # leave the loop if the user says 'stop'
+    if [[ "$REPLY" == stop ]]; then break; fi
+
+    # complain if no file was selected, and loop to ask again
+    if [[ "$filename" == "" ]]
+    then
+        echo "'$REPLY' is not a valid number"
+        continue
+    fi
+
+    # now we can use the selected file
+    echo "$filename installed"
+
+    # it'll ask for another unless we leave the loop
+    break
+done
