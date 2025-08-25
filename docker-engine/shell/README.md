e8d548886c5f42061f1992e46ebb94ba5f5499ec
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-08-25 23:51:36 +0300

    IDEA Remote development

---
+++ docker-engine/shell/README.md
@@ -0,0 +1,4 @@
+#### How to Parse Command Line Arguments in Bash
+https://stackabuse.com/how-to-parse-command-line-arguments-in-bash/
+
+
---
+++ docker-engine/shell/dialog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# https://www.geeksforgeeks.org/linux-unix/shell-scripting-dialog-boxes/
+# apt-get install dialog
+
+dialog --clear \
+--backtitle "Backtitle here" \
+--title "Title here" \
+--menu "Choose one of the following options:" 15 40 4 \
+1 "Option 1" \
+2 "Option 2" \
+3 "Option 3"
