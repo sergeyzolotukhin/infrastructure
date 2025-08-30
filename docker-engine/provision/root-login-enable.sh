db8e78a8f641a76b3ea51f52d23d567e17a554e4
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-08-30 20:51:21 +0300

    IDEA Remote development

--- docker-engine/provision/root-login-enable.sh
+++ docker-engine/provision/root-login-enable.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Enable login as root / root
 cp /vagrant/provision/etc/ssh/sshd_config.d/60-cloudimg-settings.conf /etc/ssh/sshd_config.d/60-cloudimg-settings.conf
 echo -e "root\nroot" | passwd root
 systemctl restart sshd.service
+
+# Install SSH key
+cat /vagrant/provision/ssh/id_ed25519_unsecured.pub >> /root/.ssh/authorized_keys
 
 echo "time : $(date +"%T")"
