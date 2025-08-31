8b1a2a5141846a5b251e7be03501b3814f0d3ae2
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-08-31 14:10:26 +0300

    IDEA Remote development

--- docker-engine/provision/root-login-enable.sh
+++ docker-engine/provision/root-login-enable.sh
@@ -7,3 +7,6 @@ systemctl restart sshd.service
 
 # Install SSH key
 cat /vagrant/provision/root/ssh/id_ed25519.pub >> /root/.ssh/authorized_keys
+
+# Disable message of days
+cp /vagrant/provision/etc/pam.d/sshd /etc/pam.d/sshd
